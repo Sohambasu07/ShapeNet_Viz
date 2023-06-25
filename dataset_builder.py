8ec62c179e04562040a6a221aa28499910557b94
yusufSLCN <yusuf.salcan@hotmail.com>
2023-06-25 14:06:47 +0200

    add obj to sdf function

--- dataset_builder.py
+++ dataset_builder.py
@@ -2,8 +2,9 @@ import os
 from utils import obj_to_tsdf
 import numpy as np
 import pickle
+import json
 
-root_folder = '../ShapeNetCore.v2/'
+root_folder = '../../ShapeNetCore.v2/'
 save_root_folder = 'dataset'
 if not os.path.exists(save_root_folder):
     os.mkdir(save_root_folder)
@@ -12,6 +13,9 @@ class_ids = {'plane': '02691156', 'chair': '03001627', 'table': '04379243'}
 
 num_points = 512
 threshold = 0.8
+dataset_info = {'num_points':num_points, 'threshold':threshold}
+with open(os.path.join(save_root_folder, 'dataset_info.json'), 'w') as fp:
+    json.dump(dataset_info, fp)
 
 for cls_name in class_ids:
 
@@ -21,7 +25,11 @@ for cls_name in class_ids:
     cls_path = os.path.join(root_folder, class_ids[cls_name])
     for i, sample_id in enumerate(os.listdir(cls_path)):
         sample_path = os.path.join(cls_path, sample_id + '/models/model_normalized.obj')
+        # try:
         tsdf = obj_to_tsdf(sample_path, num_points, threshold)
+        # except FloatingPointError:
+        #     print('Cannot convert to sdf')
+        #     continue
 
         tsdf_save_path = os.path.join(class_save_folder, f'{cls_name}_{i}.pkl')
         tsdf_sample = {'tsdf':tsdf, 'model_path':sample_path}
--- utils.py
+++ utils.py
@@ -1,8 +1,10 @@
 import numpy as np
 import trimesh
+import trimesh.proximity as prox
 import matplotlib.pyplot as plt
 import matplotlib.colors as colors
 import matplotlib.cm as cmx
+import math
 
 def createCmap(sequence, cmap='viridis'):
     cmap = cmx.get_cmap(cmap)
@@ -30,3 +32,14 @@ def truncated_sdf(sdf, threshold):
     # sdf[sdf < -threshold] = -threshold
     # return sdf
     return np.clip(sdf, -threshold, threshold)
+
+def obj_to_tsdf(obj_path, num_points, threshold):
+    mesh = trimesh.load_mesh(obj_path)
+    meshes = mesh.dump(concatenate=True)
+    merged_mesh = trimesh.util.concatenate(meshes)
+    gen = np.linspace(-1, 1, math.ceil(num_points**(1/3)))
+    points3D = np.array([np.array([x, y, z]) for x in gen for y in gen for z in gen])
+
+    sdf = prox.signed_distance(merged_mesh, points3D)
+    tsdf = truncated_sdf(sdf, threshold)
+    return tsdf
