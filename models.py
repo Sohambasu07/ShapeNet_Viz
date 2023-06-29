c82d5bccb5d4a4cd7fd718be59c2ff45bcff73e8
yusufSLCN <yusuf.salcan@hotmail.com>
2023-06-29 12:23:18 +0200

    fix vq quantizer function

--- models.py
+++ models.py
@@ -2,15 +2,16 @@ import torch
 import numpy as np
 
 def VQ(enc_embed : torch.Tensor, codebook : torch.Tensor):
-    #encoder embed: batch_size x Nz
+    #encoder embed: batch_size x block_count x Nz
     #codebook: KxNz
     similarity = enc_embed @ codebook.T
-    codebook_idxs = torch.argmax(similarity, dim=1)
+    codebook_idxs = torch.argmax(similarity, dim=-1)
     return codebook_idxs
 
 if __name__ == '__main__':
     cont_idxs = np.random.choice(5, 10)
     encoder_embed = torch.eye(5)[cont_idxs]
+    encoder_embed = np.repeat(encoder_embed.unsqueeze(0), 3, axis=0)
     print(encoder_embed.shape)
     print(cont_idxs)
     codebook = torch.eye(5)
